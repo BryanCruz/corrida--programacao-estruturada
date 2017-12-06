12966c9b3cdb64f9ec3a80078aa8c8741c38b9b3
bryancruz <bryan.bialokur@gmail.com>
2017-12-06 13:47:18 -0200

    atualizando rascunho

--- atividade.c
+++ atividade.c
@@ -41,32 +41,33 @@ instrucao nova_instrucao(double v, double fracao, char tipo){
       exit(1);
     }
 
-    i->v;
+    i->v = v;
     i->fracao = fracao;
     i->tipo = tipo;
     i->prox = NULL;
     return i;
 }
 int main(){
+  while(scanf() > 0){
     instrucao instrucoes = NULL;
-    char qual_v, tipo, frac1, frac2;
-    while(scanf("%c", &qual_v) > 0 && qual_v != '\n'){
-        scanf("%c", &frac1);
-        scanf("%c", &frac2);
-        scanf("%c", &tipo);
+    char qual_v, tipo, frac1, frac2, temp;
+    do{
+      scanf("%c %c %c %c", &qual_v, &frac1, &frac2, &tipo);
 
-        double v = qual_v == 'A'? 5 : 10;
-        double fracao = (1.0*(frac1 - '0'))/(frac2 - '0');
-        instrucao i = nova_instrucao(v, fracao, tipo);
-        i->prox = instrucoes;
-        instrucoes = i;
-    }
+      double v = qual_v == 'A'? 5 : 10;
+      double fracao = (1.0*(frac1 - '0'))/(frac2 - '0');
+      instrucao i = nova_instrucao(v, fracao, tipo);
+      i->prox = instrucoes;
+      instrucoes = i;
+
+    }while(scanf("%c", &temp) && temp != '\n');
 
     while(instrucoes != NULL){
-        printf("v: %.2lf\nfrac: %.2lf\ntipo: %c\n\n", instrucoes->v, instrucoes->fracao, instrucoes->tipo);
+        printf("v: %.2lf\nfrac: %.2lf\ntipo: %c\n\n",
+              instrucoes->v, instrucoes->fracao, instrucoes->tipo);
         instrucoes = instrucoes->prox;
     }
-
+  }
 
 	return 0;
 }
